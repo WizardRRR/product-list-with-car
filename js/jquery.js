ff09692fc101b26b783c2f1132a49624e9e2e124
Royer Rutti Ramos <ruttiramosroyer@gmail.com>
2024-09-03 18:08:23 -0500

    feat: :sparkles: add util jquery

---
+++ js/jquery.js
@@ -0,0 +1,17 @@
+/**
+ * Selección un único elemento HTML del DOM utilizando
+ * un selector CSS dentro del contexto especificado.
+ *
+ * @param {string} selector
+ * @param {Document | Element} [context]
+ */
+var $ = (selector, context = document) => context.querySelector(selector);
+/**
+ * Selecciona todos los elementos HTML que coincidan con
+ * el selector CSS dentro del contexto especificado.
+ *
+ * @param {string} querySelector
+ * @param {Document|Element} [context]
+ */
+var $$ = (selector, context = document) =>
+  Array.from(context.querySelectorAll(selector));
